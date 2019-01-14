ddfc435e0449315294d113caf0c4a97354520a44
Valentyn Maleta <v.maleta@bvblogic.com>
2019-01-14 15:51:29 +0200

    add link for detail about product

--- products/blueprint.py
+++ products/blueprint.py
@@ -7,6 +7,10 @@ products = Blueprint('products', __name__, template_folder='templates')
 
 @products.route('/')
 def index():
-    products=Products.query.all()
+    products = Products.query.all()
     return render_template('products/index.html',products=products)
 
+@products.route('/<id>')
+def product_detail(id):
+    product = Products.query.filter(Products.id==id).first()
+    return render_template('products/product_detail.html',product=product)
